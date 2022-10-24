edc5f6f80735af390b4f374445490f95dc573634
L2ncE <llance_24@foxmail.com>
2022-10-24 10:50:55 +0800

    feat: add cors middleware

--- apis/user-web/initialize/router.go
+++ apis/user-web/initialize/router.go
@@ -1,6 +1,7 @@
 package initialize
 
 import (
+	middlewares "E-commerce-system/apis/user-web/middleware"
 	userRouter "E-commerce-system/apis/user-web/router"
 	"fmt"
 	"github.com/cloudwego/hertz/pkg/app/server"
@@ -10,6 +11,9 @@ func Routers(port int) *server.Hertz {
 	Router := server.Default(
 		server.WithHostPorts(fmt.Sprintf(":%d", port)),
 	)
+
+	//配置跨域
+	Router.Use(middlewares.Cors())
 
 	ApiGroup := Router.Group("/v1")
 	userRouter.InitUserRouter(ApiGroup)

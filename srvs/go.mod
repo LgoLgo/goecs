37a40ff699597876a94886972ec2a74f02b3f32d
L2ncE <llance_24@foxmail.com>
2022-10-27 17:36:29 +0800

    feat: add consul and health check

--- srvs/go.mod
+++ srvs/go.mod
@@ -6,6 +6,8 @@ require (
 	github.com/anaskhan96/go-password-encoder v0.0.0-20201010210601-c765b799fd72
 	github.com/fsnotify/fsnotify v1.6.0
 	github.com/golang/protobuf v1.5.2
+	github.com/hashicorp/consul/api v1.12.0
+	github.com/satori/go.uuid v1.2.0
 	github.com/spf13/viper v1.13.0
 	go.uber.org/zap v1.23.0
 	google.golang.org/grpc v1.50.1
@@ -15,11 +17,22 @@ require (
 )
 
 require (
+	github.com/armon/go-metrics v0.3.10 // indirect
+	github.com/fatih/color v1.13.0 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-hclog v1.2.0 // indirect
+	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
+	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/hashicorp/serf v0.9.7 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
--- srvs/user_srv/main.go
+++ srvs/user_srv/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/hashicorp/consul/api"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	"google.golang.org/grpc/health/grpc_health_v1"
 	"net"
+	"srvs/user_srv/global"
 
 	"srvs/user_srv/handler"
 	"srvs/user_srv/initialize"
@@ -30,9 +34,58 @@ func main() {
 	if err != nil {
 		panic("failed to listen:" + err.Error())
 	}
+	// 注册服务健康检查
+	grpc_health_v1.RegisterHealthServer(server, health.NewServer())
+
+	//服务注册
+	cfg := api.DefaultConfig()
+	cfg.Address = fmt.Sprintf("%s:%d", global.ServerConfig.ConsulInfo.Host,
+		global.ServerConfig.ConsulInfo.Port)
+
+	client, err := api.NewClient(cfg)
+	if err != nil {
+		panic(err)
+	}
+	//生成对应的检查对象
+	check := &api.AgentServiceCheck{
+		GRPC:                           fmt.Sprintf("10.20.244.204:%d", *Port),
+		Timeout:                        "5s",
+		Interval:                       "5s",
+		DeregisterCriticalServiceAfter: "15s",
+	}
+
+	//生成注册对象
+	registration := new(api.AgentServiceRegistration)
+	registration.Name = global.ServerConfig.Name
+	registration.ID = global.ServerConfig.Name
+	registration.Port = *Port
+	registration.Tags = []string{"L2ncE", "user", "srv"}
+	registration.Address = "10.20.244.204"
+	registration.Check = check
+
+	err = client.Agent().ServiceRegister(registration)
+	if err != nil {
+		panic(err)
+	}
 
 	err = server.Serve(lis)
 	if err != nil {
 		panic("failed to start grpc:" + err.Error())
 	}
+	//
+	//go func() {
+	//	err = server.Serve(lis)
+	//	if err != nil {
+	//		panic("failed to start grpc:" + err.Error())
+	//	}
+	//}()
+	//
+	////接收终止信号
+	//quit := make(chan os.Signal)
+	//signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	//<-quit
+	//if err = client.Agent().ServiceDeregister(serviceID); err != nil {
+	//	zap.S().Info("注销失败")
+	//}
+	//zap.S().Info("注销成功")
 }

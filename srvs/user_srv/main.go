06b82412e55d9d03213cafd60704a81da4462c16
L2ncE <llance_24@foxmail.com>
2022-11-01 14:40:38 +0800

    feat: add graceful shutdown

--- srvs/user_srv/main.go
+++ srvs/user_srv/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"os/signal"
 	"srvs/user_srv/utils"
+	"syscall"
 
 	"net"
 
 	"github.com/hashicorp/consul/api"
+	uuid "github.com/satori/go.uuid"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health"
@@ -64,7 +68,8 @@ func main() {
 	//生成注册对象
 	registration := new(api.AgentServiceRegistration)
 	registration.Name = global.ServerConfig.Name
-	registration.ID = global.ServerConfig.Name
+	serviceID := fmt.Sprintf("%s", uuid.NewV4())
+	registration.ID = serviceID
 	registration.Port = *Port
 	registration.Tags = []string{"L2ncE", "user", "srv"}
 	registration.Address = "10.20.244.204"
@@ -79,20 +84,20 @@ func main() {
 	if err != nil {
 		panic("failed to start grpc:" + err.Error())
 	}
-	//
-	//go func() {
-	//	err = server.Serve(lis)
-	//	if err != nil {
-	//		panic("failed to start grpc:" + err.Error())
-	//	}
-	//}()
-	//
-	////接收终止信号
-	//quit := make(chan os.Signal)
-	//signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	//<-quit
-	//if err = client.Agent().ServiceDeregister(serviceID); err != nil {
-	//	zap.S().Info("注销失败")
-	//}
-	//zap.S().Info("注销成功")
+
+	go func() {
+		err = server.Serve(lis)
+		if err != nil {
+			panic("failed to start grpc:" + err.Error())
+		}
+	}()
+
+	// 接收终止信号
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	if err = client.Agent().ServiceDeregister(serviceID); err != nil {
+		zap.S().Info("sign out failed")
+	}
+	zap.S().Info("sign out success")
 }

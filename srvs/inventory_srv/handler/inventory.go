c816580922ceffa7ac849b58a2a2468a79aa54f3
L2ncE <llance_24@foxmail.com>
2022-11-13 21:53:21 +0800

    chore: delete comments

--- srvs/inventory_srv/handler/inventory.go
+++ srvs/inventory_srv/handler/inventory.go
@@ -30,7 +30,6 @@ func (*InventoryServer) SetInv(ctx context.Context, req *proto.GoodsInvInfo) (*e
 	global.DB.Where(&model.Inventory{Goods: req.GoodsId}).First(&inv)
 	inv.Goods = req.GoodsId
 	inv.Stocks = req.Num
-
 	global.DB.Save(&inv)
 	return &emptypb.Empty{}, nil
 }
@@ -47,9 +46,6 @@ func (*InventoryServer) InvDetail(ctx context.Context, req *proto.GoodsInvInfo) 
 }
 
 func (*InventoryServer) Sell(ctx context.Context, req *proto.SellInfo) (*emptypb.Empty, error) {
-	//扣减库存， 本地事务 [1:10,  2:5, 3: 20]
-	//数据库基本的一个应用场景：数据库事务
-	//并发情况之下 可能会出现超卖 1
 	client := goredislib.NewClient(&goredislib.Options{
 		Addr: "127.0.0.1:6379",
 	})
@@ -57,10 +53,7 @@ func (*InventoryServer) Sell(ctx context.Context, req *proto.SellInfo) (*emptypb
 	rs := redsync.New(pool)
 
 	tx := global.DB.Begin()
-	//m.Lock() //获取锁 这把锁有问题吗？  假设有10w的并发， 这里并不是请求的同一件商品  这个锁就没有问题了吗？
 
-	//这个时候应该先查询表，然后确定这个订单是否已经扣减过库存了，已经扣减过了就别扣减了
-	//并发时候会有漏洞， 同一个时刻发送了重复了多次， 使用锁，分布式锁
 	sellDetail := model.StockSellDetail{
 		OrderSn: req.OrderSn,
 		Status:  1,
@@ -73,52 +66,35 @@ func (*InventoryServer) Sell(ctx context.Context, req *proto.SellInfo) (*emptypb
 		})
 
 		var inv model.Inventory
-		//if result := tx.Clauses(clause.Locking{Strength: "UPDATE"}).Where(&model.Inventory{Goods:goodInfo.GoodsId}).First(&inv); result.RowsAffected == 0 {
-		//	tx.Rollback() //回滚之前的操作
-		//	return nil, status.Errorf(codes.InvalidArgument, "没有库存信息")
-		//}
-
-		//for {
 		mutex := rs.NewMutex(fmt.Sprintf("goods_%d", goodInfo.GoodsId))
 		if err := mutex.Lock(); err != nil {
 			return nil, status.Errorf(codes.Internal, "获取redis分布式锁异常")
 		}
 
 		if result := global.DB.Where(&model.Inventory{Goods: goodInfo.GoodsId}).First(&inv); result.RowsAffected == 0 {
-			tx.Rollback() //回滚之前的操作
+			tx.Rollback()
 			return nil, status.Errorf(codes.InvalidArgument, "没有库存信息")
 		}
-		//判断库存是否充足
+
 		if inv.Stocks < goodInfo.Num {
-			tx.Rollback() //回滚之前的操作
+			tx.Rollback()
 			return nil, status.Errorf(codes.ResourceExhausted, "库存不足")
 		}
-		//扣减， 会出现数据不一致的问题 - 锁，分布式锁
+
 		inv.Stocks -= goodInfo.Num
 		tx.Save(&inv)
 
 		if ok, err := mutex.Unlock(); !ok || err != nil {
 			return nil, status.Errorf(codes.Internal, "释放redis分布式锁异常")
 		}
-		//update inventory set stocks = stocks-1, version=version+1 where goods=goods and version=version
-		//这种写法有瑕疵，为什么？
-		//零值 对于int类型来说 默认值是0 这种会被gorm给忽略掉
-		//if result := tx.Model(&model.Inventory{}).Select("Stocks", "Version").Where("goods = ? and version= ?", goodInfo.GoodsId, inv.Version).Updates(model.Inventory{Stocks: inv.Stocks, Version: inv.Version+1}); result.RowsAffected == 0 {
-		//	zap.S().Info("库存扣减失败")
-		//}else{
-		//	break
-		//}
-		//}
-		//tx.Save(&inv)
 	}
 	sellDetail.Detail = details
-	//写selldetail表
+
 	if result := tx.Create(&sellDetail); result.RowsAffected == 0 {
 		tx.Rollback()
 		return nil, status.Errorf(codes.Internal, "保存库存扣减历史失败")
 	}
-	tx.Commit() // 需要自己手动提交操作
-	//m.Unlock() //释放锁
+	tx.Commit()
 	return &emptypb.Empty{}, nil
 }
 
